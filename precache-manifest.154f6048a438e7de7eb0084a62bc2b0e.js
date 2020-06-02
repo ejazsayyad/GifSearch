07832cde89374807275ec66969832ee076ce126b
ejazsayyad <ejazsayyad4@gmail.com>
2020-06-02 18:21:58 +0530

    Updates

---
+++ precache-manifest.154f6048a438e7de7eb0084a62bc2b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6401ed54e99dc34cb4f637f44fad9ef4",
+    "url": "/GifSearch/index.html"
+  },
+  {
+    "revision": "8e81c7f1b12828b8887c",
+    "url": "/GifSearch/static/css/main.350929e8.chunk.css"
+  },
+  {
+    "revision": "35b1e4efde7e830c2b73",
+    "url": "/GifSearch/static/js/2.57903c91.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/GifSearch/static/js/2.57903c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e81c7f1b12828b8887c",
+    "url": "/GifSearch/static/js/main.c2689fed.chunk.js"
+  },
+  {
+    "revision": "4445e3b2aec615472781",
+    "url": "/GifSearch/static/js/runtime-main.0b915a7a.js"
+  }
+]);
